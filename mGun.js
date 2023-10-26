59b96e6a7cf90df6519fcfa19608f80fc5317120
miniGun-js <141360182+miniGun-js@users.noreply.github.com>
2023-10-26 20:31:53 +0200

    new node structure and indexddb storage added

--- mGun.js
+++ mGun.js
@@ -1,25 +1,20 @@
 const
-mGun = function(opts = {}) {
-    if(mGun['$']) {
-        console.log("Singleton! Return the hidden instance...")
-        return mGun['$']
+miniGun = () => {
+    if(miniGun['$']) {
+        return miniGun['$']()
     }
     const
-    mGunSEA = mSEA,
-    dateNow = Date.now,
-    Gun = class{},
-    User = class{},
-    Contact = class{},
-    cache = {},
-    apiFeaturesBaseGun = [ 'get', 'put', 'set', 'map', 'on', 'off', 'once' ],
-    protectedGunProperties = ['#', '>'],
-    privateGunProperties = ['!', '$', '?'],
-    specialGunProperties = [...protectedGunProperties, ...privateGunProperties],
-    // options
-    GunPathDelimiter = opts.delimiter || '.',
-    globalRoot = opts.root || '~global',
-    generateSoul = opts.genSoul ? opts.genSoul : () => '#' + crypto.randomUUID(),
-    xTfilter = opts.xTfilter ? opts.xTfilter : (queue, topic) => queue.filter(task => {
+    // mSEA Crypto implementation
+    _SEA = miniGun.SEA = mSEA,
+    // path of User Root Node
+    rootStore = 'global',
+    rootPath = '~',
+    // path delimiter
+    delimiter = '.', 
+    // UUID generator
+    //generateSoul = () => '#' + crypto.randomUUID(),
+    // pub sub task instance
+    xTfilter = (queue, topic) => queue.filter(task => {
         //console.log("FILTER Wildcard", topic, task.topic)
         if(task.topic == topic || (task.topic.endsWith('*') && topic.startsWith(task.topic.slice(0, -1)))) {
             return true
@@ -27,241 +22,208 @@ mGun = function(opts = {}) {
             return false
         }
     }),
-    // pub sub task instance
-    xTmGun = new xT('mGun', { filter: xTfilter }),
+    _xT = miniGun.xT = new xT('mGun', { filter: xTfilter }),
 
-    createGunItem = function(inputObject, features = apiFeaturesBaseGun, baseObject = new Gun()) {
-        if(cached = cache[inputObject['#'].path]) {
-            console.log("CACHED", cached)
-            return cached
-        }
-        let
-        path = inputObject['#'].path,
-        isGunNode = inputObject instanceof Gun,
-        targetObject = isGunNode ? inputObject : Object.assign(
-            baseObject, 
-            { 
-                ['#']: {}, 
-                //['>']: {}, 
-                //['!']: {}, 
-                ['?']: features
-            }, 
-            inputObject 
-        ),
-        gunObjHandler = {
-            set(target, property, value) {
-                //checkIfProtectedGunProperties(property)
-                if(specialGunProperties.includes(property)) {
-                    return 
+    storeSchema = { keyPath: ['path', 'key'] },
+    storeIndexes = [
+        [ 'path', 'path', {} ],
+        [ 'time', 'time', {} ]
+    ],
+    _DB = IDB2('miniGun'),
+
+    prxHander = {
+        get: (target, property) => {
+            if(property == '>') {
+                return (prop) => target[prop] ? target[prop].time : -1
+            } else if(target['?'].includes(property)) {
+                return (...args) => _xT.emit(target, property, ...args)
+            }
+            return target[property] ? target[property].val : undefined
+        },
+        set: (target, property, value) => {
+            if(!target['!'] || (target[property] && target[property].ro)) throw new Error("READONLY")   // not logged in OR ReadOnly property
+            if(!target[property] || (target[property] && target[property].val != value)) {
+                console.log("CHANGE", property, target[property]?.val, value, target[property]?.time)
+                target[property] = {
+                    key: property,
+                    val: value,
+                    time: Date.now()
                 }
-                target[property] = value
-                target['>'][property] = dateNow()
-                //console.log("UPDATE", property, value, target, this) 
-            },
-            deleteProperty(target, property) {
-                //checkIfProtectedGunProperties(property)
-                if(specialGunProperties.includes(property)) {
-                    return 
-                }
-                //console.log("DELETE", property, target[property], target)
-                delete target[property]
-                target['>'][property] = dateNow()
-            },
-            get(target, property) {
-                //console.log("GET", property, target)
-                if(privateGunProperties.includes(property)) {
-                    return 
-                } else if(protectedGunProperties.includes(property)) {
-                    return (prop) => target[property][prop] || -1
-                } else if(target['?'].includes(property)) {
-                    return (...args) => xTmGun.emit(target, property, ...args)
-                } else if(target['__']) {
-                    // handle primitives as workaround...
-                    // https://stackoverflow.com/a/69195285/3470092
-                    let 
-                    prim = Reflect.get(target, '__'),
-                    value = prim[property]
-                    return typeof value === 'function' ? value.bind(prim) : value;
-                }
-                return target[property]
-            },
-            ownKeys(target) {
-                // filter out special properties
-                return Reflect.ownKeys(target).filter(key => !specialGunProperties.includes(key))
+                target['>'].add(property)
             }
         },
-        mGunObjectProxy = new Proxy(targetObject, gunObjHandler)
-        // trigger timestamp if unset
-        if(!isGunNode) {
-            for (prop in mGunObjectProxy) {
-                if(mGunObjectProxy['>'](prop) < 0) {
-                    mGunObjectProxy[prop] = mGunObjectProxy[prop]   
-                }
+        deleteProperty(target, property) {
+            if(!target['!'] || (target[property] && target[property].ro)) throw new Error("READONLY")
+            console.log("DELETE", property, target[property].time)
+            target[property] = {
+                val: null,
+                time: Date.now()
             }
+            target['>'].add(property)
+        },
+        ownKeys: (target) => {
+            return Reflect.ownKeys(target).filter(key => !['!', '>', '?', '#'].includes(key) && target[key].val != null)
         }
-        // cache and return proxied object 
-        return cache[path] = mGunObjectProxy
     },
 
-    mGunAPI = {
-        cache: function(path) {
-            return path ? cache.get(path) : cache
-        },
-        // Storage adapter
-        load: function(...args) {
-            // get as array of (single) property OR property => value pairs
-            // return primitive OR mGun proxied Node / Gun object
-            console.log(arguments.callee.name, args, this['#'], this)
-        },
-        save: function(...args) {
-            // object -> convert to primitives path + property => value !
-            // primitive -> save as path => value + metadata (timestamp!)
-            console.log(arguments.callee.name, args, this['#'], this)
-        },
-        merge: function(toMergeNode) {
+    createProxy = (obj) => {
+        return new Proxy(obj, prxHander)
+    },
+
+    apiFeatures = {
+        // storage
+        load: async function(table, path) {
+            console.log(arguments.callee.name, table, path, this)
             let 
-            proxied = cache[this['#'].path] = createGunItem(this),
-            currentTime = dateNow()
-            console.log(arguments.callee.name, args, this['#'], this)
-            // merge two mGun Nodes by timestamps...
-            //toMergeNode['>'].forEach(prop => {
-            for(prop in toMergeNode['>']) {
-                if(toMergeNode['>'][prop] > currentTime) {
-                    console.log('ERROR', `Timestamp ${toMergeNode['>'][prop]} is in the future...`)
-                    continue // ignore timestamp in future!!!
-                } else if(toMergeNode['>'][prop] > proxied['>'](prop)) {
-                    if(prop in toMergeNode) {
-                        this[prop] = toMergeNode[prop]
-                    } else {
-                        delete node[prop]
-                    }
-                    this['>'][prop] = toMergeNode['>'][prop]
-                } 
+            store = _DB(table, storeSchema, storeIndexes),
+            result = await store.get(path, 'path')
+            for(item of result) {
+                let 
+                sig = item.sig
+                pub = await _SEA.importKey(item.pub)
+                delete(item.sig)
+                if(await _SEA.verify(JSON.stringify(item), sig, pub)) {
+                    console.log('LOAD', "Signatures matching")
+                } else {
+                    throw new Error('LOAD', "Signature mismatch!", item, sig, pub)
+                }
             }
+            return result
         },
-        // mGun base API features
-        /**
-         * @todo single node vs nodelist ?
-         * @todo load / cache Nodes?
-         * @todo load from database...
-         */
-        get: function(path) {
-            console.log(arguments.callee.name, path, this['#'], this)
-            path = this['#'].path + GunPathDelimiter + path
-            /*if(cache[path]) {
-                return cache[path]
-            }*/
-            return createGunItem( { ['#']: { path: path }, ['>']: {} } )
-        },
-        put: function(value) {
-            let proxied = cache[this['#'].path] = createGunItem(this)   // this isn't PROXY !!! And cache written object...
-            console.log(arguments.callee.name, value, this['#'], this, proxied)
-            if(value.constructor !== Object) { // primitive
-                console.log("PRIMITIVE")
-                proxied.__ = value
-                cache[this['#'].path] = proxied
-            } else if(value['>'] || value instanceof Gun) {
-                console.log("mGun compatible")
-                proxied.merge(value)
+        save: async function() {
+            let
+            store = _DB(this['#'], storeSchema, storeIndexes),
+            //parts = Reflect.ownKeys(this).map(key => !['!', '>', '?', '#'].includes(key) ? this[key] : null).filter(notEmpty => notEmpty)
+            parts = [...this['>']].map(key => this[key])
+            for(item of parts) {
+                item.path = this['~']
+                item.pub = this['§'].pub
+                item.sig = await _SEA.sign(JSON.stringify(item), this['!'].priv, this['!'].pub)
+            }
+            this['>'] = new Set()
+            if(parts.length > 0) {
+                store.set(parts)
             } else {
-                console.log("merge simple JS Object")
-                Object.keys(value).forEach(prop => proxied[prop] = value[prop])
+                console.log("SAVE::SKIP", "No changes!")
             }
-            // save to database
-            return proxied
         },
-        set: function(item) {
-            console.log(arguments.callee.name, item, this['#'], this)
+        sync: function() {
+            console.log(arguments.callee.name, this)
         },
-        map: function(callback) {
-            console.log(arguments.callee.name, callback, this['#'], this)
+        // base 
+        get: async function(path) {
+            if(path[0] != rootPath) {
+                path = this['~'] + delimiter + path
+            }
+            let 
+            res = await _xT.emit(this, 'load', this['#'], path),
+            node = {
+                '#': this['#'],
+                '~': path,
+                '!': this['!'],
+                '§': this['§'],
+                '>': new Set(),
+                '?': [ 'put', 'map', 'on', 'off', 'save' ]
+            }
+            res.forEach(prop => node[prop.key] = prop)
+            console.log(arguments.callee.name, this['#'], path, res, node, this)
+            return createProxy(node)
         },
-        once: function(callback) {
-            console.log(arguments.callee.name, callback, this['#'],this)
+        put: function() {
+            console.log(arguments.callee.name, this)
         },
-        /**
-         * @todo single node vs nodelist ?
-         */
-        on: function(callback) {
-            console.log(arguments.callee.name, callback, this['#'],this)
+        map: function() {
+            console.log(arguments.callee.name, this)
+        },
+        on: function() {
+            console.log(arguments.callee.name, this)
         },
         off: function() {
-            console.log(arguments.callee.name, this['#'], this)
+            console.log(arguments.callee.name, this)
         },
-        // mSEA API features
-        user: async function(alias, restorePair) {
+        // user
+        /**
+         * @todo alias optional (existing user gets alias from DB)
+         */
+        user: async function(restorePair, initialAlias) {
             let 
-            bornTime = dateNow(),
-            pair = restorePair ? await mGunSEA.restore(restorePair) : await mGunSEA.pair(),
-            pub = await mGunSEA.exportKey(pair.pub),
-            epub = await mGunSEA.exportKey(pair.epub),
-            path = '~' + pub
+            bornTime = Date.now(),
+            pair = restorePair ? await _SEA.restore(restorePair) : await _SEA.pair(),
+            pub = await _SEA.exportKey(pair.pub),
+            epub = await _SEA.exportKey(pair.epub, 'epub'),
+            existingUser = res = await _xT.emit(this, 'load', pub, rootPath)
+            if(existingUser.length < 1) {
+                existingUser = {
+                    pub: { key: 'pub', val: pub, time: bornTime, ro: 1 },
+                    epub: { key: 'epub', val: epub, time: bornTime, ro: 1 },
+                    alias: { key: 'alias', val: initialAlias ? initialAlias : pub, pub, time: bornTime },
+                    '>': new Set(['pub', 'epub', 'alias'])
+                }
+            } else {
+                let node = {}
+                existingUser.forEach(prop => node[prop.key] = prop)
+                node['>'] = new Set()
+                existingUser = node
+            }
+            // auth session...
+            this['!'] = pair
+            this['§'] = { pub: pub, epub: epub }
             /**
-             * @todo get public params and borntime from storage / remote
+             * @todo remove testing save / load 
              */
-            // set alias...
-            alias = alias ? alias : pub
-            return cache[path] = createGunItem(
-                {
-                    ['#']: { soul: path, path: path },
-                    ['>']: { pub: bornTime, epub: bornTime, alias: bornTime },
-                    ['!']: pair,
-                    pub: pub,
-                    epub: epub,
-                    alias: alias
-                },
-                [ /*...apiFeaturesBaseGun*/'get', 'encrypt', 'decrypt', 'sign', 'verify', 'export', 'contact' ],
-                new User()  // base object / type
-            )
+            this['?'] = [ 'get', 'encrypt', 'decrypt', 'sign', 'verify', 'export', 'contact', 'save', 'load' ]
+            // create user 
+            return createProxy({ 
+                ...existingUser,
+                '?': this['?'],
+                '#': pub,
+                '~': rootPath,
+                '!': this['!'],
+                '§': this['§']
+            })
         },
         sign: async function(data) {
-            return await mGunSEA.sign(data, this['!'].priv)
+            return await _SEA.sign(data, this['!'].priv)
         },
         verify: async function(data, signature, foreignPub = this['!'].pub) {
             if(foreignPub instanceof CryptoKey == false) {
-                foreignPub = await mGunSEA.importKey(foreignPub)
+                foreignPub = await _SEA.importKey(foreignPub)
             }
-            return await mGunSEA.verify(data, signature, foreignPub)
+            return await _SEA.verify(data, signature, foreignPub)
         },
-        /**
-         * @todo 1:n group encryption ? 
-         */
         encrypt: async function(data, foreignPub = this['!'].epub) {
             if(foreignPub instanceof CryptoKey == false) {
-                foreignPub = await mGunSEA.importKey(foreignPub, 'epub')
+                foreignPub = await _SEA.importKey(foreignPub, 'epub')
             }
-            return await mGunSEA.encrypt(data, await mGunSEA.secret(foreignPub, this['!'].epriv))
+            return await _SEA.encrypt(data, await _SEA.secret(foreignPub, this['!'].epriv))
         },
         decrypt: async function(encData, foreignPub = this['!'].epub) {
             if(foreignPub instanceof CryptoKey == false) {
-                foreignPub = await mGunSEA.importKey(foreignPub, 'epub')
+                foreignPub = await _SEA.importKey(foreignPub, 'epub')
             }
-            let secret = await mGunSEA.secret(foreignPub, this['!'].epriv)
-            return await mGunSEA.decrypt(encData, secret)
+            let secret = await _SEA.secret(foreignPub, this['!'].epriv)
+            return await _SEA.decrypt(encData, secret)
         },
         export: async function() {
-            return await mGunSEA.backup(this['!'])
+            return await _SEA.backup(this['!'])
         },
         contact: async function(pubPair) {  // pub, epub, alias?
-            // create a object referenced to current user and the foreign "contact" (epub, pub, graph root node, alias) user
-            // 
-            return createGunItem(
-                {
-                    ['#']: { path: '~' + pubPair.pub, soul: '~' + pubPair.pub }, // contacts pub, epub, alias, -> node ?
-                    ['!']: { 
-                        pub: await mGunSEA.importKey(pubPair.pub), epub: await mGunSEA.importKey(pubPair.epub, 'epub'), // foreign contact values
-                        priv: this['!'].priv, epriv: this['!'].epriv                                                    // user priv self
-                    }, 
-                }, 
-                [ 'get', 'sign', 'verify', 'encrypt', 'decrypt' ], 
-                new Contact () 
-            )
         }
     }
 
     // initial run one-time populate API tasks
-    Object.entries(mGunAPI).forEach(([feature, action]) => xTmGun.on(feature, action))
+    Object.entries(apiFeatures).forEach(([feature, action]) => _xT.on(feature, action))
 
-    // one-time initalization finished, return mGun root object
-    return mGun['$'] = createGunItem( {['#']: { path: globalRoot, soul: globalRoot } }, [ /*...apiFeaturesBaseGun*/'get', 'user', 'cache' ] )
+    // initialization finished...
+    miniGun['$'] = () => createProxy( { 
+        '#': rootStore,         // DB table
+        '~': rootPath,          // item path
+        '>': new Set(),         // changed property tracker
+        '?': [ 'user', 'get' ]  // features
+        //'!': {}               // logged in users CryptoKey pair
+        //'§': { pub, epub }    // exported logged in users pub and epub
+    })
+
+    // expose base object
+    return miniGun['$']()
 }
